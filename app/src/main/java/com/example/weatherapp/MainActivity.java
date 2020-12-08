a2afc4bfae6f80286362c412c31974278981c775
CameronBGassedat <cameron.gassedat@ynov.com>
2020-12-08 09:18:01 +0100

    [ADD] AndroidManifest, MainActivity -> catch of weather json

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,56 +1,72 @@
 package com.example.weatherapp;
-
+import android.content.Context;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    Context context;
+    EditText editText;
+    TextView textView;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        context = getApplicationContext();
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void click(View v) {
+        String url = "https://www.prevision-meteo.ch/services/json/" + editText.getText();
+        RequestQueue queue = Volley.newRequestQueue(context);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+                        try {
+                            JSONObject jObj = new JSONObject(response);
+                            JSONObject jObjCurrent = jObj.getJSONObject("current_condition");
+                            String tmp = jObjCurrent.getString("tmp");
+                            String condition = jObjCurrent.getString("condition");
+                            String icon = jObjCurrent.getString("icon");
 
-        return super.onOptionsItemSelected(item);
+                            textView.setText("Condition :" + condition);
+                            textView2.setText("Température :" + tmp);
+                            //ImageView imageView = findViewById(R.id.imageView);
+                            //Picasso.get().load(icon).into(imageView);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("That didn't work!");
+                    }
+                });
+
+        queue.add(stringRequest);
     }
 }
