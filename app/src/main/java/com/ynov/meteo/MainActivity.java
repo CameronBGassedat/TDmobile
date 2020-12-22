0f066fb4a02f1d5444869d3dcc30a5ff076baee8
Timothée Cognard <timothee.cognard@ynov.com>
2020-12-22 17:29:25 +0100

    [ADD] functional display

--- app/src/main/java/com/ynov/meteo/MainActivity.java
+++ app/src/main/java/com/ynov/meteo/MainActivity.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
-Button button;
+FloatingActionButton button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
